1e8343319cc8d45ba17624f69e3ea592f485dc7e
marsevilspirit <marsevilspirit@gmail.com>
2024-08-13 20:24:59 +0800

    fix catch

--- mars_logger.h
+++ mars_logger.h
@@ -80,7 +80,7 @@ public:
         try{
             log = LogHead(level) + fmt::format(fmt, args...) + LogDetail(file_name, func_name, line_no);
         } catch (const std::exception& e) {
-            std::cerr << "Error in log format: " << e.what() << '\n';
+            std::cerr << "Error in log format: " << e.what() << " in " << __FILE__ << " at line " << __LINE__ << '\n';
             return;
         }
 
